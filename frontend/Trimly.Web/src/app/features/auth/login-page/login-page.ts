c5aabb283802a86702d281d681b7c977be8c4c78
Sisonke Msezane <sisonkemondli@gmail.com>
2026-02-04 00:20:39 +0200

    Login page

--- frontend/Trimly.Web/src/app/features/auth/login-page/login-page.ts
+++ frontend/Trimly.Web/src/app/features/auth/login-page/login-page.ts
@@ -1,35 +1,50 @@
 import {Component, model} from '@angular/core';
-import {form} from '@angular/forms/signals';
 import {FormGroup, ReactiveFormsModule} from '@angular/forms';
-import {FormlyFieldConfig, FormlyModule} from '@ngx-formly/core';
+import {FieldArrayType, FormlyFieldConfig, FormlyModule} from '@ngx-formly/core';
 import {InputFieldType} from '../../../shared/components/form-components/input-field-type/input-field-type';
+import {NgIcon, provideIcons} from '@ng-icons/core';
+import {svglGoogle} from '@ng-icons/svgl';
 
 @Component({
   selector: 'app-login-page',
   imports: [
     ReactiveFormsModule,
-    FormlyModule
+    FormlyModule,
+    NgIcon
   ],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
+  viewProviders: [provideIcons({ svglGoogle })]
 })
 export class LoginPage {
-
+  googleIcon = 'svglGoogle'
   form = new FormGroup({});
-  model = { email: 'email@gmail.com' };
+  model = { email: '' };
   fields: FormlyFieldConfig[] = [
     {
       key: 'email',
       type: InputFieldType,
       props: {
-        label: 'Email address',
-        placeholder: 'Enter email',
+        placeholder: 'Email',
+        icon: 'matfEmailUncolored',
+        type: 'email'
       },
-      className:'flex bg-gray-100 rounded-lg'
+      className:'flex rounded-lg'
+    },
+    {
+      key: 'password',
+      type: InputFieldType,
+      props: {
+        placeholder: 'Password',
+        icon: 'matPassword',
+        type: 'password'
+      },
+      className:'flex rounded-lg'
     }
   ];
 
   onSubmit() {
     console.log(model);
+
   }
 }
--- frontend/Trimly.Web/src/app/shared/components/form-components/input-field-type/input-field-type.ts
+++ frontend/Trimly.Web/src/app/shared/components/form-components/input-field-type/input-field-type.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {FieldType, FieldTypeConfig, FormlyAttributes} from '@ngx-formly/core';
 import {ReactiveFormsModule} from '@angular/forms';
-import {IconType, NgIcon} from '@ng-icons/core';
+import {IconType, NgIcon, provideIcons} from '@ng-icons/core';
+import {matfEmailColored} from '@ng-icons/material-file-icons/colored';
+import {matfEmailUncolored} from '@ng-icons/material-file-icons/uncolored';
+import {matPassword} from '@ng-icons/material-icons/baseline';
+import {bootstrapEye, bootstrapEyeSlash} from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-input-field-type',
   imports: [
     ReactiveFormsModule,
     FormlyAttributes,
-    NgIcon
+    NgIcon,
   ],
   template : `
-    <label class="input input-sm focus:outline-0 min-w-72 rounded-md">
-      <ng-icon [name]="iconType"></ng-icon>
-      <input type="search" class="grow" placeholder="Search"  [formControl]="formControl" [formlyAttributes]="field"/>
+    <label class="input input-sm outline-0  bg-gray-50 min-w-64 lg:min-w-72 rounded-md mb-2">
+      @if(field.props['icon']){
+        <ng-icon class="text-lg h-3!" [name]="field.props['icon']"></ng-icon>
+      }
+
+      <input [type]="field.props.type" class="grow border-0 outline-0 m-0" [formControl]="formControl" [formlyAttributes]="field"/>
+
+      @if(field.props.type==='password'){
+        <ng-icon class="text-lg h-3! cursor-pointer" [name]="'bootstrapEye'"></ng-icon>
+      }
     </label>
   `,
-  styles:''
+  styles:'',
+  viewProviders: [provideIcons({ matfEmailColored, matfEmailUncolored, matPassword , bootstrapEye, bootstrapEyeSlash})]
 })
 export class InputFieldType extends FieldType<FieldTypeConfig>{
-    iconType:IconType = 'matfEmailUncolored';
 }
